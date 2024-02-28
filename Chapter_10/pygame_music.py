bed12f1a33c97bed7ab6cd9284ef734124210a89
SuminoRyuji <sumino.ryuji@gmail.com>
2024-02-29 00:32:55 +0900

    Chapter_10

---
+++ Chapter_10/pygame_music.py
@@ -0,0 +1,51 @@
+import pygame
+import sys
+
+FILE = "./Chapter_10/"
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+CYAN = (0, 255, 255)
+
+def main():
+    pygame.init()
+    pygame.display.set_caption("初めてのPygame　サウンド出力")
+    screen = pygame.display.set_mode((800, 600))
+    clock = pygame.time.Clock()
+    font = pygame.font.Font(None, 40)
+    
+    try:
+        pygame.mixer.music.load(FILE + "pygame_bgm.ogg")
+        se = pygame.mixer.Sound(FILE + "pygame_se.ogg")
+    except:
+        print("oggファイルが見当たらないか、オーディオ機器が接続されていません")
+    
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+        
+        key = pygame.key.get_pressed()
+        if key[pygame.K_p] == 1:
+            if pygame.mixer.music.get_busy() == False:
+                pygame.mixer_music.play(-1)
+        
+        if key[pygame.K_s] == 1:
+            if pygame.mixer.music.get_busy() == True:
+                pygame.mixer_music.stop()
+        
+        if key[pygame.K_SPACE] == 1:
+            se.play()
+        
+        pos = pygame.mixer.music.get_pos()
+        txt1 = font.render("BGM pos" + str(pos), True, WHITE)
+        txt2 = font.render("[P]lay bgm : [S]top bgm : [SPACE] se", True, CYAN)
+        screen.fill(BLACK)
+        screen.blit(txt1, [100, 100])
+        screen.blit(txt2, [100, 200])
+        pygame.display.update()
+        clock.tick(10)
+
+if __name__ == "__main__":
+    main()
